223e963a399b2f76b7c05aad9a1b9431fc30b9e7
Predare <predare12000@gmail.com>
2022-04-01 16:16:34 +0300

    hided django key

--- test_nft_integrator/test_nft_integrator/settings.py
+++ test_nft_integrator/test_nft_integrator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from ..nft_api.libs.EnvVarsExtractor.VariablesList import django_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r4tzypwm6qbgru0y4n&3h5_g9xf$^n#1cfllle9%#_tvnoh28'
+SECRET_KEY = django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
